1e981166d9121aceb0b86e1c9a8e627f6fb5103f
kamc <kamsikodi@gmail.com>
2020-01-11 17:50:42 +0100

    removed unused files

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,6 @@
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-Route::get('/', 'HomeController@index');
-
-Route::resource('contacts', 'ContactController');
+Route::get('/', function () {
+    return view('welcome');
+});
