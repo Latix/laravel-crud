75ed8a86b5442c6f8615ec98f957688441cbe5d0
kamc <kamsikodi@gmail.com>
2020-01-12 17:47:09 +0100

    Implemented Access Control

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -9,6 +9,16 @@ use DB;
 class PostsController extends Controller
 {
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['index', 'show']]);
+    }
+
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
@@ -76,6 +86,10 @@ class PostsController extends Controller
     public function edit($id)
     {
         $post = Post::find($id);
+        // Check for correct user
+        if (auth()->user()->id !== $post->user_id):
+            return redirect('/posts')->with('error', 'Unauthorized Page');
+        endif;
         return view('posts.edit')->with('post', $post);
     }
 
@@ -109,6 +123,12 @@ class PostsController extends Controller
     public function destroy($id)
     {
         $post = Post::find($id);
+
+        // Check for correct user
+        if (auth()->user()->id !== $post->user_id):
+            return redirect('/posts')->with('error', 'Unauthorized Page');
+        endif;
+        
         $post->delete();
         return redirect('./posts')->with('error', 'Post Removed!');
     }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,12 +8,16 @@
     </div>
     <hr>
     <small>Written on {{$post->created_at}} by {{$post->user->name}}</small>
-    <form action="/posts/{{$post->id}}" method="POST">
-        {{csrf_field()}}
-        {{method_field('DELETE')}}
-        <div class="create-btn">
-            <a href="/posts/{{$post->id}}/edit" class="btn btn-primary">Edit</a>
-            <input type="submit" name="btn" class="btn btn-danger" value="Delete" />
-        </div>
-    </form>
+    @if (!AUth::guest())
+        @if(Auth::user()->id == $post->user->id)
+            <form action="/posts/{{$post->id}}" method="POST">
+                {{csrf_field()}}
+                {{method_field('DELETE')}}
+                <div class="create-btn">
+                    <a href="/posts/{{$post->id}}/edit" class="btn btn-primary">Edit</a>
+                    <input type="submit" name="btn" class="btn btn-danger" value="Delete" />
+                </div>
+            </form>
+        @endif
+    @endif
 @endsection
